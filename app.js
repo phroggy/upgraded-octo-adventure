abcc6138d3ce8d3406bc421e30ede3742b2058d7
Cameron Leach <cameron.developer@gmail.com>
2019-03-22 11:13:20 -0700

    Fix timezone issue for parsed date strings Move mongo connection settings to environment vars for prod deployments

--- app.js
+++ app.js
@@ -53,11 +53,3 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
-
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/"
-
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("octo");
-});
--- routes/events.js
+++ routes/events.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/"
+var url = process.env.MONGODB_URI || "mongodb://localhost:27017/"
 
 const DB_NAME = "octo";
 const COLLECTION_NAME = "events";
@@ -10,11 +10,14 @@ var tomorrow = new Date();
 tomorrow.setDate(tomorrow.getDate()+1);
 
 // Seed with some fake events for now for testing
+var new_years = new Date("2019-01-01");
+new_years.setMinutes(new_years.getMinutes()+new_years.getTimezoneOffset());
 var db_seed = [
-  { _id: 1, name: 'Fake Event 1 2019-01-01', location: "Hollywood Bowl", event_date: new Date("2019-01-01"), created_at: new Date(), modified_at: new Date()},
+  { _id: 1, name: 'Fake Event 1 2019-01-01', location: "Hollywood Bowl", event_date: new_years, created_at: new Date(), modified_at: new Date()},
   { _id: 2, name: 'Fake Event 2 (Tomorrow)', location: "Hollywood Bowl", event_date: tomorrow, created_at: new Date(), modified_at: new Date()}
 ];
 
+console.log("Connecting to MongoDB at: " + url);
 MongoClient.connect(url, function(err, client) {
   if (err) throw err;
   var dbo = client.db(DB_NAME);
@@ -64,7 +67,11 @@ router.get('/:date', function(req, res, next) {
   var date = req.params.date;
   console.log("Events for: " + date);
   // returnDate(date, req, res);
-  getEvents(new Date(date), req, res);
+  var local_date = new Date(date);
+  console.log("Parsed date: " + local_date);
+  local_date.setMinutes(local_date.getMinutes() + local_date.getTimezoneOffset());
+  console.log("Local date: " + local_date);
+  getEvents(local_date, req, res);
 });
 
 function returnDate(value, req, res) {
