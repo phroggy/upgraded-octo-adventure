3fdc539e4a5b0abfe49c9538be1682dffcc93100
Hoshizaki, Ellie <Ellie.Hoshizaki@disney.com>
2019-03-29 22:16:04 -0700

    Old scss stuff

--- app.js
+++ app.js
@@ -24,8 +24,10 @@ app.use(cookieParser());
 app.use(sassMiddleware({
   src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
-  indentedSyntax: true, // true = .sass and false = .scss
-  sourceMap: true
+  indentedSyntax: false, // true = .sass and false = .scss
+  sourceMap: true,
+  debug: true,
+  outputStyle: 'compressed'
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
