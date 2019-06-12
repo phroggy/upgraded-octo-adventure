cc2874c5d0522c921b3ef70e91352a997a9587be
Hoshizaki, Ellie <Ellie.Hoshizaki@disney.com>
2019-06-11 21:49:15 -0700

    GH-15 Create homepage styles file

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'a hollywood bowl concert tonight' });
+  var context = {
+    title: 'a hollywood bowl concert tonight',
+    class: 'home'
+  };
+
+  res.render('index', context);
 });
 
 module.exports = router;
