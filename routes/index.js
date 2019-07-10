44858abf802c20800cca07bac2a0970db7db7fd2
Hoshizaki, Ellie <Ellie.Hoshizaki@disney.com>
2019-07-10 15:11:15 -0700

    GH-15 Update homepage title

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var context = {
+  const context = {
     title: 'a hollywood bowl concert tonight',
     class: 'home'
   };
